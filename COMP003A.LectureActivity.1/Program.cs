aad2a79e209db2170424077d0e8abdc600914f4c
AngelGodlove <godlove.aj@gmail.com>
2025-01-15 19:56:49 -0800

    Add project files.

---
+++ COMP003A.LectureActivity.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003A.LectureActivity_1
+{// class level
+    internal class Program
+    {// main level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
