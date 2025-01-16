05413cb5182f3a09c4180482f78821d0eb234082
AngelGodlove <godlove.aj@gmail.com>
2025-01-15 20:14:15 -0800

    added author, course, faculty and purpose comments

--- COMP003A.LectureActivity.1/Program.cs
+++ COMP003A.LectureActivity.1/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace COMP003A.LectureActivity_1
+﻿/*
+ * Author: Angel Godlove
+ * Course: COMP-003A
+ * Faculty: Jonathan Cruz
+ * Purpose: Basic console app
+ */
+namespace COMP003A.LectureActivity_1
 {// class level
     internal class Program
     {// main level
